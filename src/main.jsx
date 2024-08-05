af43e55c097550e97b3bb6310d54ba86b4cf200d
Sufairath C <sufairath.c@gmail.com>
2024-08-05 23:26:05 +0400

    updation

--- src/main.jsx
+++ src/main.jsx
@@ -30,8 +30,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     {/* <AuthProvider> */}
+     <AuthProvider>
         <RouterProvider router={router} />
-     {/* </AuthProvider> */}
+     </AuthProvider>
   </React.StrictMode>,
 )
